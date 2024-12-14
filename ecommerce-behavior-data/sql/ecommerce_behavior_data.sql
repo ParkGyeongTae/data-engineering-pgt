dd6d4420ca5f825341026a01fb99f18c5ac21d91
GoofyPark <gt.park@pet-friends.co.kr>
2024-12-14 20:19:03 +0900

    ecommerce_behavior_data 테이블 설명 구체적으로 변경

--- ecommerce-behavior-data/sql/ecommerce_behavior_data.sql
+++ ecommerce-behavior-data/sql/ecommerce_behavior_data.sql
@@ -1,13 +1,13 @@
 CREATE EXTERNAL TABLE `bronze`.`ecommerce_behavior_data`(
-  `event_time` timestamp COMMENT '이벤트 발생 시간',
-  `event_type` string COMMENT '이벤트 타입',
+  `event_time` timestamp COMMENT '이벤트가 발생한 시간 (UTC 기준)',
+  `event_type` string COMMENT '한 가지 종류의 이벤트만 포함됨: 구매(purchase)',
   `product_id` int COMMENT '상품 ID',
-  `category_id` bigint COMMENT '카테고리 ID',
-  `category_code` string COMMENT '카테고리 코드',
-  `brand` string COMMENT '브랜드',
-  `price` double COMMENT '가격',
-  `user_id` int COMMENT '사용자 ID',
-  `user_session` string COMMENT '사용자 세션')
+  `category_id` bigint COMMENT '상품의 카테고리 ID',
+  `category_code` string COMMENT '상품의 카테고리 분류 코드(가능한 경우). 주로 의미 있는 카테고리에 존재하며, 다양한 액세서리 종류에서는 생략됨',
+  `brand` string COMMENT '소문자로 표시된 브랜드 이름 문자열, 생략될 수 있음',
+  `price` double COMMENT '상품의 가격(float), 반드시 포함됨',
+  `user_id` int COMMENT '영구 사용자 ID',
+  `user_session` string COMMENT '임시 사용자 세션 ID. 각 사용자 세션마다 동일하며, 사용자가 긴 시간 동안 온라인 스토어에 다시 접속할 때마다 변경됨')
 COMMENT '[데이터플랫폼] 전자상거래 행동 데이터'
 PARTITIONED BY (
   `year` string COMMENT '파티션 연도 yyyy',
