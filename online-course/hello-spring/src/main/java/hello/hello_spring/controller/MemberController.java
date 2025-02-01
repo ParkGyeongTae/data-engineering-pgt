1ee37673d1d9564baa60ef304e6f46a94a1bf993
박경태 <pgt0409@gmail.com>
2025-02-01 11:00:05 +0900

    회원가입 서비스 Bean 추가

---
+++ online-course/hello-spring/src/main/java/hello/hello_spring/controller/MemberController.java
@@ -0,0 +1,20 @@
+package hello.hello_spring.controller;
+
+import hello.hello_spring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+/**
+ * 스프링 실행시 컨테이너가 생성됨.
+ * 컴포넌트 스캔으로 올라감.
+ */
+@Controller
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+}
